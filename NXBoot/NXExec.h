e64b53751f4d5588f00b17ed3db3299354ed5202
ras-rap <120177313+ras-rap@users.noreply.github.com>
2026-04-05 04:33:03 -0500

    Fix NXExec linkage for ObjC++ (extern "C" + prototype)

--- NXBoot/NXExec.h
+++ NXBoot/NXExec.h
@@ -1,8 +1,15 @@
+#import <Foundation/Foundation.h>
+
+@class NXUSBDevice;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Existing NXExec function declarations here
+BOOL NXExec(NXUSBDevice *device,
+            NSData *relocator,
+            NSData *payload,
+            NSString * _Nullable * _Nullable error);
 
 #ifdef __cplusplus
 }
